81429a5a62bf38d3c7393d35073ad7c2182e428e
BRTEK007 <bartoszkonderla@gmail.com>
2025-10-18 18:30:59 +0200

    day 6 part 2 attempt

--- sol_6.cpp
+++ sol_6.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <cstdlib>
 #include <vector>
+#include <algorithm>
 
 std::pair<int, int> read_input(std::vector<std::vector<char>>& matrix_out){
 	matrix_out.clear();
@@ -78,14 +79,30 @@ int part_one(){
 }
 
 int part_two(){
+	// Guard is in a loop if he returns to the path he was on
+	// Every time you cross a path he was on, check if on obstacle can be placed to return to that path
+	// The path taken should be extended to the closest obstacles in both ways
+	/*
+		#		#
+		|		UP
+		^	->	UP
+		.		UP
+		#		#
+	*/
+	
 	std::vector<std::vector<char>> matrix;
+
+	constexpr char DIR_FLAGS[] = {0b00000001, 0b00000010, 0b00000100, 0b00001000};
+
+	static_assert((('#' & 0b11110000) != 0) && (('.' & 0b11110000) != 0),
+	"Flag combinations collide with other markings!");
 
 	auto guard_pos = read_input(matrix);
 	int gx, gy;
 	gx = guard_pos.first;
 	gy = guard_pos.second;
 
-	int dirs[][2] = {{0,-1}, {1,0}, {0,1}, {-1,0}};
+	int dirs[][2] = {{0,-1}, {1,0}, {0,1}, {-1,0}};//UP RIGHT DOWN LEFT
 	int dir_index = 0;
 
 	int width = matrix[0].size();
@@ -93,28 +110,91 @@ int part_two(){
 
 	int possible_obstacles = 0;
 
+	std::vector<std::pair<int,int>> new_obs;
+
 	bool searching = true;
 	while(searching){
-		if(matrix[gy][gx] != 'X'){
-			unique_steps++;
-			matrix[gy][gx] = 'X';
+		int next_dir = (dir_index+1)%4;
+		int opposite_dir = (dir_index+2)%4;
+
+		
+
+		int next_x = gx + dirs[dir_index][0];
+		int next_y = gy + dirs[dir_index][1];
+
+		bool is_next_pos_valid = (next_x >= 0 && next_x < width && next_y >= 0 && next_y < height);
+
+		if(matrix[gy][gx] == '.' || matrix[gy][gx] == '^'){// mark path for the first time
+			matrix[gy][gx] = DIR_FLAGS[dir_index];
+		}else if(matrix[gy][gx] & DIR_FLAGS[next_dir]){ // can loop
+			//crossing path, check for possible loop
+			if(is_next_pos_valid && matrix[next_y][next_x] == '.'){
+				possible_obstacles++;
+
+				std::cout<<"\n"<<((matrix[gy][gx] & DIR_FLAGS[0]) > 0)<<" "<<
+								((matrix[gy][gx] & DIR_FLAGS[1]) > 0)<<" "<<
+								((matrix[gy][gx] & DIR_FLAGS[2]) > 0)<<" "<<
+								((matrix[gy][gx] & DIR_FLAGS[3]) > 0)<<" ";
+
+				std::cout<<"\nobstacle #"<<new_obs.size()<<" val: "<<(int)matrix[gy][gx]<<" , dirs: "<<dir_index<<" "<<next_dir;
+
+				new_obs.push_back({next_x, next_y});
+			}
 		}
+		matrix[gy][gx] |= DIR_FLAGS[dir_index];
 
-		gx += dirs[dir_index][0];
-		gy += dirs[dir_index][1];
-
-		if(gx < 0 || gx >= width || gy < 0 || gy >= height){
+		if(!is_next_pos_valid){
 			searching = false;
 			break;
 		}
 
-		if(matrix[gy][gx] == '#'){//obstacle change dir
-			gx -= dirs[dir_index][0];
-			gy -= dirs[dir_index][1];
+		if(matrix[next_y][next_x] == '#'){//obstacle change dir
 
-			dir_index = dir_index == 3 ? 0 : dir_index+1;
+			//fully mark entire availible space in the row/column
+			int ox = gx;
+			int oy = gy;
+			while(true){
+				ox += dirs[opposite_dir][0];
+				oy += dirs[opposite_dir][1];
+				
+				if(ox < 0 || ox >= width || oy < 0 || oy >= height)
+					break;
+
+				if(matrix[oy][ox] == '#')
+					break;
+
+				if(matrix[oy][ox] == '.')//first marking, reset
+					matrix[oy][ox] = 0;
+
+				matrix[oy][ox] |= DIR_FLAGS[dir_index];//mark direction
+			}
+
+			dir_index = next_dir;//change dir
+		}else{
+			gx = next_x;
+			gy = next_y;
 		}
 	}
+
+
+	for(auto &row : matrix){
+		for(auto &c : row){
+			if(c != '.' && c != '#'){
+				c = '.';
+			}
+		}
+	}
+
+	for(auto pos : new_obs){
+		matrix[pos.second][pos.first] = 'O';
+	}
+
+	std::cout<<'\n';
+	for(const auto &row : matrix){
+		std::string str(row.begin(), row.end());
+		std::cout<<str<<'\n';
+	}
+	std::cout<<'\n';
 
 	return possible_obstacles;
 }
