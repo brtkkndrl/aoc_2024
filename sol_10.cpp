a9d12de5c89cf9f66f2cf29e95fdfe1c0c63928d
BRTEK007 <bartoszkonderla@gmail.com>
2025-10-26 22:21:49 +0100

    day 2 part 2

--- sol_10.cpp
+++ sol_10.cpp
@@ -88,5 +88,51 @@ int part_one(){
 }
 
 int part_two(){
-	return 0;
+		std::vector<std::vector<char>> matrix;
+	std::vector<std::pair<int,int>> trailheads;
+
+	read_input(matrix, trailheads);
+
+	int width = matrix[0].size();
+	int height = matrix.size();
+
+	auto is_pos_valid = [width, height](int x, int y)
+	{
+		return x >= 0 && x < width && y >= 0 && y < height;
+	};
+
+	const int DIRS[][2] = {{0,-1}, {1,0}, {0,1}, {-1,0}};
+
+	int score_sum = 0;
+
+	for(const auto &trailhead : trailheads){
+		std::stack<std::pair<int,int>> frontier;
+		frontier.push(trailhead);
+
+		while(!frontier.empty()){
+			const auto curr_node = frontier.top();
+			frontier.pop();
+
+			for(const auto &dir : DIRS){
+				int next_x = curr_node.first + dir[0];
+				int next_y = curr_node.second + dir[1];
+				
+				if(!is_pos_valid(next_x, next_y))
+					continue;
+
+				if(matrix[next_y][next_x] != matrix[curr_node.second][curr_node.first] + 1)
+					continue;
+				
+				if(matrix[next_y][next_x] == 9){
+					score_sum++;
+					continue;
+				}
+
+				frontier.push({next_x, next_y});
+			}
+		}
+
+	}
+
+	return score_sum;
 }
